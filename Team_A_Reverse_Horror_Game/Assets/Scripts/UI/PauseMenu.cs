da2f07a4da5f0bb7b99dbf599200682b6f8c8a23
Melvilla <aidan.melville99@gmail.com>
2025-06-28 15:15:19 +0100

    Pause Menu Fixed
    Fixed pause menu and added additional functionality

--- Team_A_Reverse_Horror_Game/Assets/Scripts/UI/PauseMenu.cs
+++ Team_A_Reverse_Horror_Game/Assets/Scripts/UI/PauseMenu.cs
@@ -5,6 +5,8 @@ public class PauseMenu : MonoBehaviour
 {
     public bool isPaused = false;
     public GameObject pauseMenu;
+    public GameObject controlMenu;
+    public GameObject exitMenu;
 
     // Update is called once per frame
     void Update()
@@ -40,6 +42,10 @@ public class PauseMenu : MonoBehaviour
 
         //Find the player controller and disable it
         FindAnyObjectByType<FirstPersonController>().enabled = false;
+
+        //Release the mouse so it can interact with the menu
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 
     //Function to Resume Game
@@ -54,9 +60,17 @@ public class PauseMenu : MonoBehaviour
         //Diable the pause menu object 
         pauseMenu.SetActive(false);
 
+        //Diable the control menu object 
+        controlMenu.SetActive(false);
+
+        //Diable the exit menu object 
+        exitMenu.SetActive(false);
+
         //Find and reactivate the player controller
         FindAnyObjectByType<FirstPersonController>().enabled = true;
+
+        //Locks the cursor so the player can take control of the character
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
-
-
 }
