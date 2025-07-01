68925d526ca8741a7b566b317fd99f1de23c452e
Melvilla <aidan.melville99@gmail.com>
2025-07-01 21:53:19 +0100

    Script & UI updates
    Updated UI controls panel to reflect correct controls. Removed unused namespaces from GameStateManager

--- Team_A_Reverse_Horror_Game/Assets/Scripts/State Manager/GameStateManager.cs
+++ Team_A_Reverse_Horror_Game/Assets/Scripts/State Manager/GameStateManager.cs
@@ -1,8 +1,6 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.Splines.ExtrusionShapes;
 
 public class GameStateManager : MonoBehaviour
 {
